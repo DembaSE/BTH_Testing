dc255dcd2a77408ccb3e2185d05e845b1ea842f5
DembaSE <kaned30@gmail.com>
2024-03-10 15:02:21 +0100

    Small fixes basket class

--- SoftareTesting/src/test/java/project/Basket.java
+++ SoftareTesting/src/test/java/project/Basket.java
@@ -2,31 +2,61 @@ package project;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a basket for managing products.
+ */
 public class Basket {
-	 private Map<String, Double> products;
+	private Map<String, Double> products;
+	
+	/**
+     * Constructs a new empty basket.
+     */
+	public Basket() {
+		products = new HashMap<String, Double>();
+	}
 
-	    public Basket() {
-	        products = new HashMap<String, Double>();
-	    }
+    /**
+     * Adds a product to the basket.
+     *
+     * @param productName the name of the product to add
+     * @param price the price of the product
+     */
 
-	    public void addProduct(String productName, double price) {
-	        products.put(productName, price);
-	    }
+	public void addProduct(String productName, double price) {
+		products.put(productName, price);
+	}
+	
+	 /**
+     * Removes a product from the basket.
+     * @param productName the name of the product to remove
+     */
 
-	    public void removeProduct(String productName) {
-	        products.remove(productName);
-	    }
+	public void removeProduct(String productName) {
+		products.remove(productName);
+	}
+	
+	 /**
+     * Gets the count of products in the basket.
+     *
+     * @return the number of products in the basket
+     */
 
-	    public int getProductCount() {
-	        return products.size();
-	    }
+	public int getProductCount() {
+		return products.size();
+	}
+	
+	/**
+     * Calculates the total price of all products in the basket.
+     *
+     * @return the total price of all products in the basket
+     */
 
-	    public double getTotalPrice() {
-	        double totalPrice = 0;
-	        for (double price : products.values()) {
-	            totalPrice += price;
-	        }
-	        return totalPrice;
-	    }
+	public double getTotalPrice() {
+		double totalPrice = 0;
+		for (double price : products.values()) {
+			totalPrice += price;
+		}
+		return totalPrice;
+	}
 
 }
